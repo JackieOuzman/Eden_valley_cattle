73dd2d9b70f343473ad73f5511f53f890e2f3645
Ouzman <ouz001@csiro.au>
2019-07-19 17:30:31 +1000

    extra stuff for filtering out data heaps to workk on still animation should run with background polygon and line

--- filtering_out_data_week1.R
+++ filtering_out_data_week1.R
@@ -380,5 +380,192 @@ VF_week1_2_InclusionBord1 %>%
 
 
 
+
+
+
+###bring in the polygon file
+eden_valley <- st_read(
+  "W:/VF/Eden_Valley/VF_Boundary/EdenValley_site1GDA_a.shp")
+eden_valley_VF_Hot <- st_read(
+  "W:/VF/Eden_Valley/VF_Boundary/Fence_hotwire_rd_side_clipped.shx")
+
+fence1 <- st_read(
+  "W:/VF/Eden_Valley/VF_Boundary/Fence1.shx")
+fence2 <- st_read(
+  "W:/VF/Eden_Valley/VF_Boundary/Fence2.shx")
+fence3 <- st_read(
+  "W:/VF/Eden_Valley/VF_Boundary/Fence3.shx")
+fence4 <- st_read(
+  "W:/VF/Eden_Valley/VF_Boundary/Fence4a.shx")
+fence5 <- st_read(
+  "W:/VF/Eden_Valley/VF_Boundary/Fence_hotwire_rd_side_clipped.shx")
+
+
+### plot the ploygon
+ggplot()+
+  geom_sf(data = eden_valley, size = 2 , colour = "black", fill=NA)+
+  geom_sf(data = fence1, size = 1 , colour = "black")+
+  geom_sf(data = fence2, size = 1 , colour = "grey")+
+  geom_sf(data = fence3, size = 1 , colour = "blue")+
+  geom_sf(data = fence4, size = 1 , colour = "red")+
+  geom_sf(data = fence5, size = 1 , colour = "pink")+
+  coord_sf()
+###subset data to messing about
+graph1_data <- VF_week1_2_3_InclusionBord %>% 
+  filter(date == "2019-05-20" & collar_ID == "ac138") 
+
+###define the coords I need to use
+mapCRS <- CRS("+init=epsg:28354")     # 28355 = GDA_1994_MGA_Zone_54
+
+### turn my data into 'shapefile' format - not sure i need this step nope I dont?
+plot_some_pts <- st_as_sf(graph1_data, coords = c("POINT_X", "POINT_Y"), crs = mapCRS)
+
+
+### This is annimation plot using data as sf
+p1 <- ggplot(plot_some_pts, aes(lon, lat)) +
+  geom_point() +
+  labs(subtitle = 'Date: {format(frame_time, "%b %e")}') +
+  transition_time(time)
+p1
+
+
+###Add the background
+p3 <- ggplot() +
+  geom_sf(data = eden_valley, color = "red") +
+  geom_point(data = plot_some_pts, aes(lon, lat), inherit.aes = FALSE) +
+  labs(subtitle = 'Date: {format(frame_time, "%b %e")}',
+       caption = "Frame {frame} of {nframes} ({progress * 100}%)") +
+  transition_time(time) +
+  shadow_wake(0.3)
+
+
+p3
+
+
+###Add the background
+p4 <- ggplot() +
+  geom_sf(data = eden_valley, color = "blue") +
+  geom_point(data = graph1_data, aes(POINT_X, POINT_Y), inherit.aes = FALSE) +
+  labs(subtitle = 'Date: {format(frame_time, "%b %e")}',
+       caption = "Frame {frame} of {nframes} ({progress * 100}%)") +
+  transition_time(time) +
+  shadow_wake(0.3)
+
+animate(p4, duration = 60) 
+
+###can split this up a bit and add in larger data set
+##subset data to messing about
+animmation_data <- VF_week1_2_3_InclusionBord %>% 
+  filter(date == "2019-05-20") 
+write.csv(animmation_data, "animmation_data.csv")
+
+p4a <- ggplot() +
+  geom_sf(data = eden_valley, color = "black", fill = NA) +
+  geom_sf(data = fence1, color = "grey") +
+  geom_sf(data = fence2, color = "grey") +
+  geom_sf(data = fence3, color = "grey") +
+  geom_sf(data = fence4, color = "grey") +
+  geom_sf(data = fence5, color = "grey") +
+  geom_point(data = animmation_data, aes(POINT_X, POINT_Y,colour = collar), inherit.aes = FALSE) +
+  #facet_wrap(.~collar)+
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.text.x=element_text(angle=90,hjust=1))
+
+  
+p4a  
+  
+p4b <- p4a +
+  labs(subtitle = 'Date: {format(frame_time, "%b %e")}',
+       caption = "Frame {frame} of {nframes} ({progress * 100}%)") +
+  transition_time(time) +
+  shadow_wake(0.3)
+
+animate(p4b, duration = 30) 
+
+
+#Things to make this better -long period
+#add other collars
+#add extra VF boundary
+####################################################################################
+VF_week1_2_3_InclusionBord %>% 
+  filter(date == "2019-05-20") %>% 
+  ggplot(aes(x = POINT_X, y = POINT_Y, colour = collar))+
+  geom_point()+
+  facet_wrap(.~collar)+
+  labs(title= "week1 2019-05-xx",
+       x= "POINT_X",
+       y = "POINT_Y")+
+  theme(axis.text.x=element_text(angle=90,hjust=1),
+        legend.position = "none")
   
 
+
+  
+### This is annimation plot using data as df
+p2 <- ggplot(graph1_data, aes(lon, lat)) +
+  geom_point() +
+  labs(subtitle = 'Date: {format(frame_time, "%b %e")}') +
+  transition_time(time)  
+
+p2
+
+
+VF_week1_2_3_InclusionBord %>% 
+  filter(date == "2019-05-20") %>% 
+  ggplot(aes(x = POINT_X, y = POINT_Y, colour = collar))+
+  geom_point()+
+  labs(title= "week1 2019-05-xx",
+       x= "POINT_X",
+       y = "POINT_Y")+
+  #,
+  #subtitle = "hour: {closest_state}",
+  #caption = "Frame {frame} of {nframes} ({progress * 100}%)")+
+  theme(axis.text.x=element_text(angle=90,hjust=1),
+        legend.position = "none")+
+  transition_time(hour)
+
+
+############################################################################################################
+#############                       Change of tack try to clip the points to my polygon ###########
+install.packages("raster")
+library(raster)
+library(sf)
+
+#point data = ###subset data to messing about
+ac204_20_05_df <- VF_week1_2_3_InclusionBord %>% 
+  filter(date == "2019-05-20" & collar_ID == "ac204") 
+
+### turn my data into 'shapefile' format - not sure i need this step ?
+ac204_20_05 <- st_as_sf(ac204_20_05_df, coords = c("POINT_X", "POINT_Y"), crs = mapCRS)
+glimpse(ac204_20_05)
+
+#polygon file = eden_valley
+
+test <- st_intersection(ac204_20_05, eden_valley)
+ggplot()+
+  geom_sf(data = eden_valley, size = 2 , colour = "black", fill=NA)+
+  geom_sf(data = fence1, size = 1 , colour = "black")+
+  geom_sf(data = fence2, size = 1 , colour = "grey")+
+  geom_sf(data = fence3, size = 1 , colour = "blue")+
+  geom_sf(data = fence4, size = 1 , colour = "red")+
+  geom_sf(data = fence5, size = 1 , colour = "pink")+
+  geom_sf(data = ac204_20_05, size = 1 , colour = "black")
+  coord_sf()
+  
+  test1 <- st_intersection(ac204_20_05, eden_valley)
+  test2 <- st_intersection(ac204_20_05, eden_valley)
+  ggplot()+
+    geom_sf(data = eden_valley, size = 2 , colour = "black", fill=NA)+
+    geom_sf(data = fence1, size = 1 , colour = "black")+
+    geom_sf(data = fence2, size = 1 , colour = "grey")+
+    geom_sf(data = fence3, size = 1 , colour = "blue")+
+    geom_sf(data = fence4, size = 1 , colour = "red")+
+    geom_sf(data = fence5, size = 1 , colour = "pink")+
+    geom_sf(data = test, size = 1 , colour = "black")
+  coord_sf()
+  
+  
+  ######################## Get my DF back???####################################################
+  
+ 
