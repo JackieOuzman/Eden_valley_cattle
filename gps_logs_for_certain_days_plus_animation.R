6f3a73d57f6c3f9bf2195f814be6c2e6a1a67de0
Ouzman <ouz001@csiro.au>
2020-02-21 15:23:13 +1100

    extra black and white gps plot grouping days together

--- gps_logs_for_certain_days_plus_animation.R
+++ gps_logs_for_certain_days_plus_animation.R
@@ -1,5 +1,5 @@
 ### Graphing the cows movement a certain days....
-
+install.packages("DT")
 library(tidyverse)
 library(dplyr)
 library(lubridate)
@@ -20,11 +20,83 @@ library(DT)
 
 graph_path <- file.path("W:", "VF", "Eden_valley", "graphs")
 
+#Bring in the data
+input_data <- file.path("W:", "VF", "Eden_valley", "logged_VF_data", "Jax_Dec_2019_processing")
+
+VF1_recal <- read_csv(paste0(input_data, "/VF1_recal.csv"))
+VF2_recal <- read_csv(paste0(input_data, "/VF2_recal.csv"))
+VF3_recal <- read_csv(paste0(input_data, "/VF3_recal.csv"))
+VF4_recal <- read_csv(paste0(input_data, "/VF4_recal.csv"))
+VF5_recal <- read_csv(paste0(input_data, "/VF5_recal.csv"))
+
 VF1_recal <- mutate(VF1_recal, VF = 1)
 VF2_recal <- mutate(VF2_recal, VF = 2)
 VF3_recal <- mutate(VF3_recal, VF = 3)
 VF4_recal <- mutate(VF4_recal, VF = 4)
 VF5_recal <- mutate(VF5_recal, VF = 5)
+
+str(VF1_recal)
+unique(VF5_recal$date)
+#need to make df into spatial object for graphing and try clipping to paddock boundary
+VF1_recal <-
+  st_as_sf(VF1_recal,
+           coords = c("X", "Y"),
+           crs = 28354,
+           agr = "constant") %>% 
+  st_intersection( eden_valley)
+
+VF2_recal <-
+  st_as_sf(VF2_recal,
+           coords = c("X", "Y"),
+           crs = 28354,
+           agr = "constant") %>% 
+  st_intersection( eden_valley)
+
+VF3_recal <-
+  st_as_sf(VF3_recal,
+           coords = c("X", "Y"),
+           crs = 28354,
+           agr = "constant") %>% 
+  st_intersection( eden_valley)
+
+VF4_recal <-
+  st_as_sf(VF4_recal,
+           coords = c("X", "Y"),
+           crs = 28354,
+           agr = "constant") %>% 
+  st_intersection( eden_valley)
+
+VF5_recal <-
+  st_as_sf(VF5_recal,
+           coords = c("X", "Y"),
+           crs = 28354,
+           agr = "constant") %>% 
+  st_intersection( eden_valley)
+
+
+########################################################################################################
+###  paddock bounadry
+eden_valley <- st_read("W:/VF/Eden_Valley/VF_Boundary/EdenValley_site1GDA_a.shp")
+#assign a coord ref epsg
+st_crs(eden_valley) <- 28354
+
+#make this the epsg that will be used for all data
+the_crs <- st_crs(eden_valley, asText = TRUE)
+
+### 9b. VF lines
+
+fence1 <- st_read("W:/VF/Eden_Valley/VF_Boundary/Fence1.shp")
+fence2 <- st_read("W:/VF/Eden_Valley/VF_Boundary/Fence2.shp")
+fence3 <- st_read("W:/VF/Eden_Valley/VF_Boundary/Fence3.shp")
+fence4 <- st_read("W:/VF/Eden_Valley/VF_Boundary/Fence4a.shp")
+fence5 <- st_read("W:/VF/Eden_Valley/VF_Boundary/Fence5.shp")
+
+st_crs(fence1) <- 28354
+st_crs(fence2) <- 28354
+st_crs(fence3) <- 28354
+st_crs(fence4) <- 28354
+st_crs(fence5) <- 28354
+
 
 
 ################################################################################################################################
@@ -34,11 +106,14 @@ head(VF1_recal)
 #Try plotting a few days day 1 vf 1
 day1Vf1 <- filter(VF1_recal, day_since_start == 1 & VF == 1) 
 head(day1Vf1)
+str(day1Vf1)
+
+
   ggplot() +
   geom_sf(data = eden_valley, color = "black", fill = NA) +
   geom_sf(data = fence1, color = "grey") +
   geom_sf(data = day1Vf1, aes(colour = animal_ID)) +
-  facet_wrap(.~animal_ID)+
+  #facet_wrap(.~animal_ID)+
   theme_bw()+
   theme(legend.position = "none",
         axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
@@ -658,3 +733,72 @@ p4b <- p4a +
 animation_20thVf1 <- animate(p4b, duration = 60) 
 animation_20thVf1
 anim_save(animation = animation_20thVf1 , filename = "animation_20thVf1.gif")
+
+
+
+
+########################################################################################################
+###########   
+
+day1_3Vf2 <- filter(VF2_recal, between(VF2_recal$day_since_start,1,3) & VF == 2)
+unique(day1_3Vf2$day_since_start) 
+
+
+
+### 1b. Day 1-3 balck and white
+ggplot() +
+  geom_sf(data = eden_valley, color = "black", fill = NA) +
+  geom_sf(data = fence2, color = "blue") +
+  geom_sf(data = day1_3Vf2, alpha = 0.01)+
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title= "Day 1, 2, 3 VF 2")+
+  xlab("") +
+  ylab("") 
+
+ggsave(path= graph_path, filename = "day1_3Vf2_BW.png", device = "png" ,
+       width = 20, height = 12, units = "cm")
+
+
+### 1c. Day 4-8 balck and white
+unique(VF3_recal$day_since_start) 
+
+day4_5Vf3 <- filter(VF3_recal, between(VF3_recal$day_since_start,4,8) & VF == 3)
+unique(day4_5Vf3$day_since_start) 
+
+ggplot() +
+  geom_sf(data = eden_valley, color = "black", fill = NA) +
+  geom_sf(data = fence3, color = "blue") +
+  geom_sf(data = day4_5Vf3, alpha = 0.01)+
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title= "Day 4 to 8 VF 3")+
+  xlab("") +
+  ylab("") 
+
+ggsave(path= graph_path, filename = "day4_8Vf3_BW.png", device = "png" ,
+       width = 20, height = 12, units = "cm")
+
+
+### 1c. Day 9-14 balck and white
+unique(VF5_recal$day_since_start) 
+
+day4_5Vf3 <- filter(VF3_recal, between(VF3_recal$day_since_start,4,8) & VF == 3)
+unique(day4_5Vf3$day_since_start) 
+
+ggplot() +
+  geom_sf(data = eden_valley, color = "black", fill = NA) +
+  geom_sf(data = fence3, color = "blue") +
+  geom_sf(data = day4_5Vf3, alpha = 0.001)+
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title= "Day 4 to 8 VF 3")+
+  xlab("") +
+  ylab("") 
+
+ggsave(path= graph_path, filename = "day4_8Vf3_BW.png", device = "png" ,
+       width = 20, height = 12, units = "cm")
+
